69804945a7575fb04badbce2a55dd33c4bf711fa
beem35 <beemmlg1234@gmail.com>
2025-11-20 13:45:05 +0700

    Lab1 fix

--- lab-1/Lab_1_10.java
+++ lab-1/Lab_1_10.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 
 public class Lab_1_10 {
-    public double calculateArea(double width,double height){
+    public static double calculateArea(double width,double height){
         return width*height   ;
     }
     public static void main(String[] args) {
         Scanner odj = new Scanner(System.in) ;
         double a = odj.nextDouble();
         double b = odj.nextDouble();
-        Lab_1_10 myodj = new Lab_1_10();
-        System.out.println("Sum : "+ myodj.calculateArea(a,b));
+        System.out.println("Sum : "+ calculateArea(a,b));
         odj.close();
     }
 }
---
+++ lab-1/Lab_1_11.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Lab_1_11 {
+    public static int sumArray(int[] num ){
+        Scanner myObj = new Scanner(System.in);
+        int sum = 0 ;
+        for(int j = 0 ; j < num.length ; j++ ){
+            num[j] = myObj.nextInt();
+            sum += num[j] ;
+        }
+        myObj.close();
+        return sum ;
+    }
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int N = myObj.nextInt() ;
+        int[] numArray = new int[N] ; 
+        
+        System.out.printf("sum : %d ",sumArray(numArray)) ;
+        myObj.close();
+    }
+}
