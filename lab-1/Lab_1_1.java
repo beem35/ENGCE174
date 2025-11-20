5ee7b8a23700a0a8f9e986f10ea41a20e9ea733c
beem35 <beemmlg1234@gmail.com>
2025-11-20 14:35:15 +0700

    l1

--- lab-1/Lab_1_1.java
+++ lab-1/Lab_1_1.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class Lab_1_1 {
     public static void main( String[] args ) {
         Scanner myObj = new Scanner( System.in ) ; // Scanner of input data
-        int numberInput_1 = myObj.nextInt() ;
-        int numberInput_2 = myObj.nextInt() ;
+
+        int numberInput_1 = myObj.nextInt() ; // input number 1
+        int numberInput_2 = myObj.nextInt() ; // input number 2
+
         System.out.println( numberInput_1 + numberInput_2 ) ; // output
+
         myObj.close() ; // close input data
+
     }//end main
 }//end Lab_1_1
