bc395e9e87decf774017b14cd6169d3b7f99dd3c
beem35 <beemmlg1234@gmail.com>
2025-11-20 14:08:21 +0700

    lab1 fix3

--- lab-1/Lab_1_1.java
+++ lab-1/Lab_1_1.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
 public class Lab_1_1 {
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         Scanner obj = new Scanner( System.in ) ;
         int num1 = obj.nextInt() ;
         int num2 = obj.nextInt() ;
-        int sum = num1 + num2 ;
-        System.out.println( sum ) ;
+        System.out.println( num1 + num2 ) ;
         obj.close() ;
     }//end main
 }//end Lab_1_1
