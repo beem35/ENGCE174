5f96d52bf364fbad6dccd03316a809bbe4aed3e9
beem35 <beemmlg1234@gmail.com>
2025-11-20 14:37:10 +0700

    l1-1

--- lab-1/Lab_1_1.java
+++ lab-1/Lab_1_1.java
@@ -4,8 +4,8 @@ public class Lab_1_1 {
     public static void main( String[] args ) {
         Scanner myObj = new Scanner( System.in ) ; // Scanner of input data
 
-        int numberInput_1 = myObj.nextInt() ; // input number 1
-        int numberInput_2 = myObj.nextInt() ; // input number 2
+        int numberInput_1 = myObj.nextInt() ; // input num1
+        int numberInput_2 = myObj.nextInt() ; // input num2
 
         System.out.println( numberInput_1 + numberInput_2 ) ; // output
 
