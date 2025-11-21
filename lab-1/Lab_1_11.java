846d11dbeff842cc1d6835f0b91097c5bcef71c2
beem35 <beemmlg1234@gmail.com>
2025-11-21 10:00:33 +0700

    lab1 ffv2

--- lab-1/Lab_1_11.java
+++ lab-1/Lab_1_11.java
@@ -5,6 +5,7 @@ public class Lab_1_11 {
         Scanner myObj = new Scanner(System.in);
         int sum = 0;
         for (int j = 0; j < num.length; j++) {
+            System.out.printf("input Number [%d]: ",j+1);
             num[j] = myObj.nextInt();
             sum += num[j];
         }
@@ -14,6 +15,7 @@ public class Lab_1_11 {
 
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
+        System.out.printf("input Number of loop : ");
         int N = myObj.nextInt();
         int[] numArray = new int[N];
 
