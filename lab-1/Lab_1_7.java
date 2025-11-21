7b3e7220d592b4c3e754075f045d7d97ac396c9f
beem35 <beemmlg1234@gmail.com>
2025-11-21 10:07:37 +0700

    lab1-7 fix

--- lab-1/Lab_1_7.java
+++ lab-1/Lab_1_7.java
@@ -4,13 +4,14 @@ public class Lab_1_7 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         System.out.printf("input number for loop : ");
-        int i = obj.nextInt();
+        int N = obj.nextInt();
         int sum = 0;
-        for (int j = 0; j < i; j++) {
+        for (int j = 0; j < N; j++) {
+            System.out.printf("input number [%d] : ",j+1);
             int num = obj.nextInt();
             sum += num;
         }
-        System.out.printf("%d", sum);
+        System.out.printf("sum : %d", sum);
         obj.close();
     }
 }
